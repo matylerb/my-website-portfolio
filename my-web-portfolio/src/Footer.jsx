60fe11067c67e713b5e8330e03614aa6039cac8a
Tyler <tylerbrady2005@gmail.com>
2025-10-31 19:52:47 +0000

    losing my mind

---
+++ my-web-portfolio/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+            <p>© {new Date().getFullYear()} My Web Portfolio. All rights reserved.</p>
+        </footer>
+    );
+}
+
+
+export default Footer
