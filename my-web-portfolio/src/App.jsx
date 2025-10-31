90bb162fe48cccf0e8d921078d02f0be1765e58b
Tyler <tylerbrady2005@gmail.com>
2025-10-31 22:42:45 +0000

    yippee

--- my-web-portfolio/src/App.jsx
+++ my-web-portfolio/src/App.jsx
@@ -1,4 +1,3 @@
-import Header from './Header.jsx'
 import Footer from './footer.jsx'
 import Hero from './Hero.jsx'
 import Projects from './Projects.jsx';
@@ -8,7 +7,6 @@ import About from './About.jsx';
 function App(){
     return(
       <>
-          <Header />
           <Hero />
           <About />
           <Projects />
